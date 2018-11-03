bfea49dbeb546d02be9115a98a96cb84ccd099b9
Donatas T <donatas.tamosauskas@gmail.com>
2018-11-03 13:49:02 +0200

    Generating images to display speed differences in low and high pixel count.

--- testing.py
+++ testing.py
@@ -1,4 +1,5 @@
 from game_engine.speed_testing import SpeedTest
 
 if __name__ == "__main__":
-    SpeedTest().plot_differences(max_x=50, step=10, loop_times=15)
+    SpeedTest().plot_differences(max_x=20, step=1, loop_times=10000, save_plot=True, filename="low_pixel_count.png")
+    SpeedTest().plot_differences(max_x=70, step=5, loop_times=100, save_plot=True, filename="high_pixel_count.png")
