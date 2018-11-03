5be847cce452661f7072f98b1be8cf644b96da4c
Donatas T <donatas.tamosauskas@gmail.com>
2018-11-03 14:05:57 +0200

    Generated images to display game engine speed differences.

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 from game_engine.speed_testing import SpeedTest
 
 if __name__ == "__main__":
-    SpeedTest().plot_differences(max_x=20, step=1, loop_times=10000, save_plot=True, filename="low_pixel_count.png")
-    SpeedTest().plot_differences(max_x=70, step=5, loop_times=100, save_plot=True, filename="high_pixel_count.png")
+    SpeedTest().plot_differences(max_x=20, step=1, loop_times=10000, save_plot=True, filename="images/low_pixel_count.png")
+    SpeedTest().plot_differences(max_x=70, step=5, loop_times=100, save_plot=True, filename="images/high_pixel_count.png")
